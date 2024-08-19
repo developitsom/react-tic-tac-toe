3cedb368677f69118593e85e0ed1202583eec8a9
Somendra Sharma <developitsom@gmail.com>
2024-08-19 14:03:54 +0530

    Creation of Tic-Tac-Toe game

---
+++ README.md
@@ -0,0 +1,19 @@
+# React Tic-Tac-Toe
+
+A simple react project for tic-tac-toe game.
+
+### Pre-Requisities:
+- React.JS
+- NodeJS & npm
+
+### How to run:
+ It can be run after cloning this repository and then run:
+ ```
+npm install
+ ```
+ Since it has `vite` configured already, all on need to do is to run this application using:
+
+```
+npm run dev
+```
+It will start the application on `http://localhost:5173`.
---
+++ src/components/Player.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing); // written as funnction beacuse if we use value directly then it might take some time to update and get the latest value
+
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
+  }
+
+  function handleChange(event) {
+    //here the event will get the current value of playerName after changing it. The way of getting the value from the input and then setting it back is called 'two-way binding'.
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input
+        type="text"
+        required
+        value={playerName}
+        onChange={handleChange}
+      ></input>
+    );
+  }
+
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{!isEditing ? "Edit" : "Save"}</button>
+    </li>
+  );
+}
